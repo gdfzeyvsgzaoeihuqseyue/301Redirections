d7bdb77803bdde2c1ab4f11ef0e94a91d2192db5
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-10-02 05:36:01 +0100

    correction

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Site déplacé</h1>
+    <p> Ce site a été déplacé vers une nouvelle adresse.</p>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+main {
+  padding: 40px;
+  text-align: center;
+}
+</style>
